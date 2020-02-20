fd6b79d40a74a683c9f5a4aa16c793471a173b9a
vcz.zhang@gmail.com <victor@iMac-Workstation-31.local>
2020-02-20 15:58:41 -0800

    basic js ready

--- battleship/src/OpponentGrid.js
+++ battleship/src/OpponentGrid.js
@@ -142,11 +142,19 @@ class OpponentGrid extends Component {
                 const ycoord = Math.floor(Math.random() * 10);
                 const potentialBoat = [];
                 for (let j = 0; j < thisShipLength; j++) {
+                    // Check if it goes off the board:
+
+                    // HERE
+
                     // Then check to see if the below position is already in it
                     const thisCoordinateString = `${xcoord}_${ycoord + j}`;
                     if (validatedPositionStrings.includes(thisCoordinateString)) {
                         // Invalid
                         continue tryShip;
+                    } 
+                    else if (ycoord + j > 9) {
+                        // Also invalid
+                        continue tryShip
                     }
                     thisBoatPossiblePositionStrings.push(thisCoordinateString);
 
@@ -160,28 +168,38 @@ class OpponentGrid extends Component {
                 // push positions to placedPosition?
                 validatedPositionStrings.push(...thisBoatPossiblePositionStrings);
                 placedPosition.push(...potentialBoat)
-                
-                // console.log(potentialBoat[0][0])
-                // console.log(potentialBoat[0][1])
-
-                // let test = placedPosition.slice(0, 5)
-
-                let a
-                for (a = 0; a < this.state.ships.length; a++) {
-                    let b
-                    for (b = 0; b < this.state.ships[a].getLength(); b++) {
-                        let x = placedPosition[b][0]
-                        let y = placedPosition[b][1]
-
-                        board[x][y] = this.state.ships[a].getName()
-                    }
-                }
-
                 break;
             }
         }
-        console.log(placedPosition)
+        
+        // console.log(placedPosition)
+        // console.log(placedPosition[0])
+        
+        let test = this.state.board.slice()
 
+        // let b
+        // for (b = 0; b < this.state.ships[0].getLength(); b++) {
+        //     console.log(b)
+        //     let xc = placedPosition[b][0]
+        //     let yc = placedPosition[b][1]
+
+        //     test[xc][yc] = this.state.ships[0].getName()
+        // }
+
+
+        let a
+        for (a=0; a < this.state.ships.length; a++) {
+            console.log(a)
+            let b
+            for (b=0; b < this.state.ships[a].getLength(); b++) {
+                console.log(b)
+                let xc = placedPosition[b][0]
+                let yc = placedPosition[b][1]
+
+                test[xc][yc] =  this.state.ships[a].getName()
+            }
+            placedPosition.splice(0, this.state.ships[a].getLength())
+        }
 
         this.setState(
             { board: board }
